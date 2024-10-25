30725a31c11af7a66d77db9ff1b21afd4e9c5b9e
yu <yul1024@outlook.com>
2024-10-25 16:40:22 +0800

    update

--- dataset/frame_dataset.py
+++ dataset/frame_dataset.py
@@ -1,20 +1,37 @@
-# from torch.utils.data import Dataset
-#
-#
-# class FrameDataset(Dataset):
-#     def __init__(self, frames, labels):
-#         pass
-#
-#     def __len__(self):
-#         return len(frames)
-#
-#     def __getitem__(self, idx):
-#         pass
+from moviepy.editor import VideoFileClip
+import pysrt
+
+from omegaconf import OmegaConf, DictConfig
+from pathlib import Path
 
 
-class Frame:
-    def __init__(self, frame):
+class Frames:
+    def __init__(self, video_id):
+        self.path_config = OmegaConf.load('../configs/path.yaml')
+        self.base_dir_path = Path(self.path_config['datasets']['base_dir'])
+
+        self.video_path, self.subtitle_path = self.get_video_and_subtitle_path(video_id)
+        self.video = self.load_video(frame)
+
+    def get_video_and_subtitle_path(self, video_id):
+        video_path = self.base_dir_path / f"{video_id}.mp4"
+        subtitle_path = self.base_dir_path / f"{video_id}.srt"
+        return video_path, subtitle_path
+
+    def load_video(self, video_path):
+        clip = VideoFileClip(video_path)
+
+    def load_subtitle(self, subtitle_path):
+        subs = pysrt.open(subtitle_path)
+
+    def get_frame_image_by_time(self, frame):
         pass
+
+    def get_frame_subtitle_by_time(self, frame):
+        pass
+
+
+
 
 
 
