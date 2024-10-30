a2e73d30157d0ec8eee910476186d37af32d8ece
yu <yul1024@outlook.com>
2024-10-30 14:44:00 +0800

    Update __init__.py

--- embedding/__init__.py
+++ embedding/__init__.py
@@ -1,9 +1,14 @@
 # __all__ = []
 
 
-# from .title_encoder import TextEncoder
+# from .title_encoder import TitleEncoder
 from .text_encoder import TextEncoder
+
 from .image_to_text import Captioner
 # from .image_encoder import ImageEncoder
+
+# from .face_extractor import FaceExtractor
+# from .facial_encoder import FacialEncoder
+
 from .audio_encoder import AudioEncoder
 
