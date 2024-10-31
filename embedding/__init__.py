24ef93da72a58bf63be79d05ef35d95205137e7e
yu <yul1024@outlook.com>
2024-10-31 21:17:38 +0800

    finished embedding

--- embedding/__init__.py
+++ embedding/__init__.py
@@ -5,9 +5,10 @@
 from .text_encoder import TextEncoder
 
 from .image_to_text import Captioner
-# from .image_encoder import ImageEncoder
 
-# from .face_extractor import FaceExtractor
+from .image_encoder import ImageEncoder
+
+from .face_extractor import FaceExtractor
 # from .facial_encoder import FacialEncoder
 
 from .audio_encoder import AudioEncoder
