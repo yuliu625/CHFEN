2cffeb09bcd480dab785d272e634f5384e5b2211
yu <yul1024@outlook.com>
2024-11-02 14:57:35 +0800

    Update model.py

---
+++ model/model.py
@@ -0,0 +1,17 @@
+# from .conditioned_image_encoder import
+# from .conditioned_text_encoder import
+
+import torch
+import torch.nn as nn
+
+from pathlib import Path
+from omegaconf import OmegaConf
+
+
+class CHFEN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == '__main__':
+    pass
