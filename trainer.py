688223302d61cb9ae67081d98eda7bca8e16384c
yu <yul1024@outlook.com>
2024-11-05 20:27:14 +0800

    update

---
+++ trainer.py
@@ -0,0 +1,28 @@
+from dataset import OriginalMultimodalDataset
+from model import CHFEN
+
+import torch
+from torch.utils.data import DataLoader
+
+from omegaconf import OmegaConf
+from pathlib import Path
+
+
+def init(cfg: OmegaConf):
+    pass
+
+
+def get_dataloader(cfg: OmegaConf):
+    pass
+
+
+def train(cfg: OmegaConf):
+    pass
+
+
+def evaluate(cfg: OmegaConf):
+    pass
+
+
+if __name__ == '__main__':
+    pass
