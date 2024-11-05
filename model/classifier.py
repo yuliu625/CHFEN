d57e2296c6b01f92bef27252d2db8dd2fa9e3243
yu <yul1024@outlook.com>
2024-11-05 20:26:00 +0800

    finished

--- model/classifier.py
+++ model/classifier.py
@@ -6,12 +6,17 @@ from omegaconf import OmegaConf
 
 
 class EmotionClassifier(nn.Module):
+    """
+    下游任务。
+    这里默认定义的是8分类任务。
+    """
     def __init__(self, input_dim=768, num_classes=8):
         super().__init__()
         self.linear = nn.Linear(input_dim, num_classes)
 
     def forward(self, x):
-        return self.linear(x)
+        out = self.linear(x)
+        return out
 
 
 if __name__ == '__main__':
