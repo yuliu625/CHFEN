bf2a0986708503e545b4378f92b29b0f9f579336
yu <yul1024@outlook.com>
2024-11-05 20:26:13 +0800

    finished

--- model/conditioned_image_encoder.py
+++ model/conditioned_image_encoder.py
@@ -1,4 +1,5 @@
 from model.projection_layer import ProjectionLayer
+from model.positional_encoding import positional_encoding
 from model.gate import ImageFusionLayer
 
 import torch
@@ -23,25 +24,72 @@ class ConditionedImageEncoder(nn.Module):
         super().__init__()
         self.image_fusion_layer = ImageFusionLayer()
 
+        self.positional_encoding = positional_encoding
+
         # 这个是conditioned query的实现。
         self.cross_attention = nn.MultiheadAttention(embed_dim=embedding_dim, num_heads=num_heads)
+
+    # def forward(self, conditioned_query_embedding, image_embeddings_input):
+    #     scene_embedding_list = image_embeddings_input['scene_embedding_list']
+    #     face_embedding_list = image_embeddings_input['face_embedding_list']
+    #
+    #     sequence_length = len(scene_embedding_list)
+    #
+    #     result_embedding_list = []
+    #     for i in range(sequence_length):
+    #         scene_embedding = scene_embedding_list[i]
+    #         num_faces, face_embedding = face_embedding_list[i]
+    #         image_embedding = self.image_fusion_layer(num_faces, scene_embedding, face_embedding)
+    #
+    #         attention_output, _ = self.cross_attention(conditioned_query_embedding, image_embedding, image_embedding)
+    #         result_embedding_list.append(attention_output)
+    #
+    #     return result_embedding_list
+
+    # def forward(self, conditioned_query_embedding, image_embeddings_input):
+    #     scene_embedding_list = image_embeddings_input['scene_embedding_list']
+    #     face_embedding_list = image_embeddings_input['face_embedding_list']
+    #
+    #     num_faces_list = [face_embedding[0] for face_embedding in face_embedding_list]
+    #     face_embedding_list_ = [face_embedding[1] for face_embedding in face_embedding_list]
+    #
+    #     scene_embedding_sequence = torch.cat(scene_embedding_list, dim=1)
+    #     face_embedding_sequence = torch.cat(face_embedding_list_, dim=0)
+    #
+    #
+    #     result_embedding_list = []
+    #     for i in range(sequence_length):
+    #         scene_embedding = scene_embedding_list[i]
+    #         num_faces, face_embedding = face_embedding_list[i]
+    #         image_embedding = self.image_fusion_layer(num_faces, scene_embedding, face_embedding)
+    #
+    #         attention_output, _ = self.cross_attention(conditioned_query_embedding, image_embedding, image_embedding)
+    #         result_embedding_list.append(attention_output)
+    #
+    #     return result_embedding_list
 
     def forward(self, conditioned_query_embedding, image_embeddings_input):
         scene_embedding_list = image_embeddings_input['scene_embedding_list']
         face_embedding_list = image_embeddings_input['face_embedding_list']
 
+        # face和scene融合，这不进行位置编码。
         sequence_length = len(scene_embedding_list)
-
-        result_embedding_list = []
+        image_result_embedding_list = []
         for i in range(sequence_length):
             scene_embedding = scene_embedding_list[i]
             num_faces, face_embedding = face_embedding_list[i]
             image_embedding = self.image_fusion_layer(num_faces, scene_embedding, face_embedding)
+            image_result_embedding_list.append(image_embedding)
 
-            attention_output, _ = self.cross_attention(conditioned_query_embedding, image_embedding, image_embedding)
-            result_embedding_list.append(attention_output)
+        # 矩阵化计算，加入位置编码。
+        image_embedding_sequence = torch.cat(image_result_embedding_list, dim=0)
+        image_embedding_sequence += positional_encoding(image_embedding_sequence.shape[0], image_embedding_sequence.shape[1])
+        conditioned_query_embedding = conditioned_query_embedding.expand_as(image_embedding_sequence)
 
-        return result_embedding_list
+        # 计算结果。
+        attention_output, _ = self.cross_attention(conditioned_query_embedding, image_embedding_sequence, image_embedding_sequence)
+
+        return attention_output
 
 
 if __name__ == '__main__':
