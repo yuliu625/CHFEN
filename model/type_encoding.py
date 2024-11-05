bbd872202997b4a0b85344869a74f15be1d859f9
yu <yul1024@outlook.com>
2024-11-05 20:26:51 +0800

    finished

--- model/type_encoding.py
+++ model/type_encoding.py
@@ -8,11 +8,16 @@ from omegaconf import OmegaConf
 
 
 class TypeEncoding(nn.Module):
-    def __init__(self):
+    """
+    每个种类的具体embedding。
+    实例化后是不同的。
+    """
+    def __init__(self, embedding_dim=768):
         super().__init__()
+        self.type_encoding = nn.Parameter(torch.randn(1, embedding_dim))
 
-    def forward(self, x):
-        pass
+    def forward(self):
+        return self.type_encoding
 
 
 if __name__ == '__main__':
