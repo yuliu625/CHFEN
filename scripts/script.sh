a72099f13067dc9e1c89de3c8e6cedb20ace27ef
yu <yul1024@outlook.com>
2024-11-07 21:06:44 +0800

    Create script.sh

---
+++ scripts/script.sh
@@ -0,0 +1,5 @@
+nohup python /home/liuyu/workspace/code/paper/news_emotion/trainer.py >/home/liuyu/nohup_out/CHFEN/lr_test/experiment_1.log 2>&1 &
+
+nohup python /home/liuyu/workspace/code/paper/news_emotion/trainer.py >/home/liuyu/nohup_out/CHFEN/lr_test/experiment_2.log 2>&1 &
+
+nohup python /home/liuyu/workspace/code/paper/news_emotion/trainer.py >/home/liuyu/nohup_out/CHFEN/lr_test/experiment_3.log 2>&1 &
