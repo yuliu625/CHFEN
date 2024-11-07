17d709b87d5a059839b3556b54460b120c9dec73
yu <yul1024@outlook.com>
2024-11-07 21:06:58 +0800

    Update model_all_test.py

--- model_all_test.py
+++ model_all_test.py
@@ -1,13 +1,17 @@
 from model import CHFEN
 from dataset import OriginalMultimodalDataset
 
+from omegaconf import OmegaConf
+
 
 if __name__ == '__main__':
-    dataset = OriginalMultimodalDataset(path_config_path_str=r'D:\dcmt\code\dl\paper\news_emotion\configs\path.yaml')
-    chfen = CHFEN()
+    config = OmegaConf.load(r'D:\dcmt\code\dl\paper\news_emotion\config_experiments\baseline_model.yaml')
+    dataset = OriginalMultimodalDataset(path_config_path_str=config['dataloader']['train']['dataset_config_path'])
+    chfen = CHFEN(config)
     out = chfen(dataset[0])
     print(out.shape)
     print(out)
+
 
 """
 torch.Size([1, 8])
