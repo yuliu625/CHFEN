702811da33cf289b425bf2ba8693a0a69fcd75a5
yu <yul1024@outlook.com>
2024-11-08 18:28:49 +0800

    Update __init__.py

--- untils/__init__.py
+++ untils/__init__.py
@@ -2,3 +2,4 @@
 
 from .image_trans import trans_np_array_to_image
 from .checkpoint_tool import load_checkpoint, save_checkpoint
+from .get_original import get_sequence_length_from_num_faces_vector
