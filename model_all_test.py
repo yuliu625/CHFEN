8cc2ecc0168095252e3e3e0e669043f5d197d422
yu <yul1024@outlook.com>
2024-11-08 18:28:36 +0800

    Update model_all_test.py

--- model_all_test.py
+++ model_all_test.py
@@ -1,16 +1,22 @@
 from model import CHFEN
-from dataset import OriginalMultimodalDataset
+from dataset import OriginalMultimodalDataset, MultimodalDataset
+from torch.utils.data import DataLoader
+from dataset import collate_fn
 
 from omegaconf import OmegaConf
 
 
 if __name__ == '__main__':
     config = OmegaConf.load(r'D:\dcmt\code\dl\paper\news_emotion\config_experiments\baseline_model.yaml')
-    dataset = OriginalMultimodalDataset(path_config_path_str=config['dataloader']['train']['dataset_config_path'])
+    # dataset = OriginalMultimodalDataset(path_config_path_str=config['dataloader']['train']['dataset_config_path'])
+    dataset = MultimodalDataset(path_config_path_str=config['dataloader']['train']['dataset_config_path'], encoder_config_path_str=r'D:\dcmt\code\dl\paper\news_emotion\config_experiments\encoder.yaml')
+    dataloader = DataLoader(dataset, batch_size=2, shuffle=False, collate_fn=collate_fn)
     chfen = CHFEN(config)
-    out = chfen(dataset[0])
-    print(out.shape)
-    print(out)
+    for batch in dataloader:
+        out = chfen(dataset[0])
+        # print(out.shape)
+        print(out)
+        break
 
 
 """
