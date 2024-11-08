dbcbe952de66020b2cbcf76fc436f82dc9fed8e5
yu <yul1024@outlook.com>
2024-11-08 18:27:39 +0800

    Update __init__.py

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,4 +1,11 @@
 # __all__ = []
 
+# 可以被dataloader进行加载的dataset
+from .batch_dataset import MultimodalDataset
+# 以及和它配备的填充方法
+from .collate_fn import collate_fn
+
+# 原始单独可正常运行的dataset
 from .processed_dataset import ProcessedMultimodalDataset
 from .original_dataset import OriginalMultimodalDataset
+
