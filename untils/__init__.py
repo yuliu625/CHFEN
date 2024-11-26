35ce6c49b80ab72dca81877be8a35f1280942446
yu <yul1024@outlook.com>
2024-11-26 13:49:27 +0800

    update

--- untils/__init__.py
+++
@@ -1,6 +0,0 @@
-# __all__ = []
-
-from .image_trans import trans_np_array_to_image
-from .checkpoint_tool import load_checkpoint, save_checkpoint
-from .get_original import get_sequence_length_from_num_faces_vector
-from .move_dict_to_device import move_batch_to_device
--- untils/move_dict_to_device.py
+++
@@ -1,4 +0,0 @@
-
-def move_batch_to_device(batch, device):
-    """如果数据是dict，然后需要将里面的tensor移动到gpu，用这个方法。"""
-    return {key: value.to(device) for key, value in batch.items()}
