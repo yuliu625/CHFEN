baa7e8d3d4c775f55edd1b0f3dc26238c85a639f
yu <yul1024@outlook.com>
2025-08-26 11:02:22 +0800

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,9 +1,9 @@
-nohup python /home/liuyu/workspace/code/paper/news_emotion/trainer.py >/home/liuyu/nohup_out/CHFEN/lr_test/experiment_1.log 2>&1 &
+nohup python /home/liuyu/workspace/code/paper/news_emotion/main.py >/home/liuyu/nohup_out/CHFEN/lr_test/experiment_1.log 2>&1 &
 
-nohup python /home/liuyu/workspace/code/paper/news_emotion/trainer.py >/home/liuyu/nohup_out/CHFEN/lr_test/experiment_2.log 2>&1 &
+nohup python /home/liuyu/workspace/code/paper/news_emotion/main.py >/home/liuyu/nohup_out/CHFEN/lr_test/experiment_2.log 2>&1 &
 
-nohup python /home/liuyu/workspace/code/paper/news_emotion/trainer.py >/home/liuyu/nohup_out/CHFEN/lr_test/experiment_3.log 2>&1 &
+nohup python /home/liuyu/workspace/code/paper/news_emotion/main.py >/home/liuyu/nohup_out/CHFEN/lr_test/experiment_3.log 2>&1 &
 
-nohup python /home/liuyu/workspace/code/paper/news_emotion/trainer.py >/home/liuyu/nohup_out/CHFEN/decision/experiment_1.log 2>&1 &
+nohup python /home/liuyu/workspace/code/paper/news_emotion/main.py >/home/liuyu/nohup_out/CHFEN/decision/experiment_1.log 2>&1 &
 
-nohup python /home/liuyu/workspace/code/paper/news_emotion/trainer.py >/home/liuyu/nohup_out/CHFEN/decision/experiment_2.log 2>&1 &
+nohup python /home/liuyu/workspace/code/paper/news_emotion/main.py >/home/liuyu/nohup_out/CHFEN/decision/experiment_2.log 2>&1 &
